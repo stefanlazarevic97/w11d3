b88df7be35bb7e02c182ccc2f6440d02e2bd894b
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-02 14:37:57 -0700

    add phase 0

--- grocery-store/src/index.js
+++ grocery-store/src/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 import './index.css';
 import App from './App';
 
-function Root() {
-  return (
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-  );
+const store = configureStore(); 
+
+if (process.env.NODE_ENV !== "production") {
+    window.store = store;
 }
 
+function Root() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    );
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
